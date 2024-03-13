93678ecd60ffb305521fcbe0517a63b02ae7d48c
Armando <armandgv16@hotmail.com>
2024-03-12 19:28:17 -0500

    Init

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,15 @@
+import PropType from "prop-types";
+
+export const GifItem = ({ title, url }) => {
+  return (
+    <div className="card">
+      <img src={url} alt={title} />
+      <p>{title}</p>
+    </div>
+  );
+};
+
+GifItem.propTypes = {
+  title: PropType.string,
+  url: PropType.string,
+};
